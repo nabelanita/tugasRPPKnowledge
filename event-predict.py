ac921066a0a9771dfcbab41438caf0f46dca930c
nabelanita <13519104@mahasiswa.itb.ac.id>
2022-11-02 23:49:58 +0700

    update template

--- event-predict.py
+++ event-predict.py
@@ -13,6 +13,17 @@ app = Flask(__name__)  # Create application object
 def index():
 	return render_template('index.html')
 
+def predict():
+	##TODO: import model, return model
+	return 0
+
+@app.route('/predict', method=['POST'])
+def result():
+	## TODO predict using model in predict(), display result
+	
+	return render_template('predict.html',prediction='test')
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)  # Run our application
